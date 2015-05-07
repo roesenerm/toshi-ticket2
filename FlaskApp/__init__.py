b311a53449e1be51b8f5cdb7caa3a0075f652469
Matthew Roesener <mattroesener@timelinelabs.com>
2015-05-07 10:34:30 -0700

    app.run()

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -310,7 +310,8 @@ def sendBitcoin(issuing_public_address, minting_fee, token):
 
 
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port)
+	#port = int(os.environ.get('PORT', 5000))
+	#app.run(host='0.0.0.0', port=port)
+	app.run()
 
 
