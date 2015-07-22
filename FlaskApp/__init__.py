ee9e8ad3ff2c7717e0bbe2de27704f590f677773
Matthew Roesener <mattroesener@timelinelabs.com>
2015-07-22 11:26:59 -0700

    changed main file

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -613,8 +613,8 @@ def metadata(asset_id):
 
 
 if __name__ == '__main__':
-	#port = int(os.environ.get('PORT', 5000))
-	#app.run(host='0.0.0.0', port=port)
-	app.run(debug=True)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
+	#app.run(debug=True)
 
 
